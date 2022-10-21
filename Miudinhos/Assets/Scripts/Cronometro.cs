ea57c651a3e0b76dab5d8919b0fb96067d6eca92
Thiago Cerqueira <thicerqs@gmail.com>
2022-10-21 11:42:27 -0300

    Adiciona checagem de medalhas

--- Miudinhos/Assets/Scripts/Cronometro.cs
+++ Miudinhos/Assets/Scripts/Cronometro.cs
@@ -7,6 +7,11 @@ public class Cronometro : MonoBehaviour
 {
     public Text timer_text;
     [HideInInspector]public float timer;
+
+    [Header("Tempo para as medalhas (segundos)")]
+    public int timeGoldenMedal;
+    public int timeSilverMedal;
+    public int timeBronzeMedal;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +23,16 @@ public class Cronometro : MonoBehaviour
     {
         timer += Time.deltaTime;
         timer_text.text = string.Format("{0:00}:{1:00}", Mathf.Floor(timer / 60), timer%60);
+    }
+
+    void CheckMedal()
+    {
+        if (timer < timeGoldenMedal) {
+            //mostra medalha de ouro
+        } else if (timer < timeSilverMedal) {
+            //mostra medalha de prata
+        } else {
+            //mostra medalha de bronze
+        }
     }
 }
