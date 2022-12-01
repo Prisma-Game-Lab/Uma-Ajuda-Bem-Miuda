f51b94a7aa1cc37870d92085395bd87106cc4b01
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2022-12-01 18:20:03 -0300

    menu correções

--- Miudinhos/Assets/Scripts/ShowMinigame.cs
+++ Miudinhos/Assets/Scripts/ShowMinigame.cs
@@ -1,37 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class ShowMinigame : MonoBehaviour
-{
-    public GameObject setaMiudo; 
-    public GameObject miudoButton;
-    public GameObject spriteMinigame;
-    public GameObject miudoSprite;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ShowMinigame : MonoBehaviour
+//{
+    //public GameObject setaMiudo; 
+    //public GameObject miudoButton;
+    //public GameObject spriteMinigame;
+    //public GameObject miudoSprite;
 
 
 
-    bool PressMiudo();     //Verifica qual miudo foi selecionado pelo player e ativa a seta abaixo dele, o sprite do minigame e o botao do minigame
-    {
-        if(miudoSprite.IsPressed())      //Boleana para verificar se o miudo esta pressionado
-        {
-            return true;
-        }
+    //bool PressMiudo();     //Verifica qual miudo foi selecionado pelo player e ativa a seta abaixo dele, o sprite do minigame e o botao do minigame
+    //{
+       //answ = miudoSprite.IsPressed();  //Boleana para verificar se o miudo esta pressionado
+       //return answ
+        
 
-    }
+
+    //}
     
-    void Update()
-    {
-        if(PressMiudo == false)
-        {
-            PressMiudo();
-        }
-        else
-        {
-            setaMiudo.SetActive();
-            miudoButton.SetActive(); 
-            spriteMinigame.SetActive();
-        }
+    //void Update();
+    //{
+        //if(PressMiudo == false)
+        //{
+           // PressMiudo();
+        //}
+        //else
+        //{
+          //  setaMiudo.SetActive();
+            //miudoButton.SetActive(); 
+            //spriteMinigame.SetActive();
+        //}
 
-    }
-}
+    //}
+//}
