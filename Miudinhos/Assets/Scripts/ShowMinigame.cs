9e7553bc321ef4893fafaa50750f95360860c1bd
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2022-12-01 17:07:14 -0300

    alterações menu

---
+++ Miudinhos/Assets/Scripts/ShowMinigame.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMinigame : MonoBehaviour
+{
+    public GameObject setaMiudo; 
+    public GameObject miudoButton;
+    public GameObject spriteMinigame;
+    public GameObject miudoSprite;
+
+
+
+    bool PressMiudo();     //Verifica qual miudo foi selecionado pelo player e ativa a seta abaixo dele, o sprite do minigame e o botao do minigame
+    {
+        if(miudoSprite.IsPressed())      //Boleana para verificar se o miudo esta pressionado
+        {
+            return true;
+        }
+
+    }
+    
+    void Update()
+    {
+        if(PressMiudo == false)
+        {
+            PressMiudo();
+        }
+        else
+        {
+            setaMiudo.SetActive();
+            miudoButton.SetActive(); 
+            spriteMinigame.SetActive();
+        }
+
+    }
+}
